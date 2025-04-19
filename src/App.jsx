c58905135b0299ea68069bf3a69d5e9f9a1212f7
protapkrghosh <protapg518@gmail.com>
2025-04-19 09:13:11 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold bg-sky-100 py-3 text-center'>Boi Poka</h1>
+    </>
+  )
+}
+
+export default App
