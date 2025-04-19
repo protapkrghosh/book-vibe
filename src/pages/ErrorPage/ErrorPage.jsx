f0f70a7fda42822f0293833fb4c82c9ff951932b
protapkrghosh <protapg518@gmail.com>
2025-04-19 10:10:19 +0600

    added navbar

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+   return (
+      <div>
+         <h1 className='text-9xl font-extrabold text-center'>404</h1>
+      </div>
+   );
+};
+
+export default ErrorPage;
